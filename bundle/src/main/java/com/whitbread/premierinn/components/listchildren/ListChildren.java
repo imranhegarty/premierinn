c4534eb2ff54723c2008b147da5146d8bbaa6cbd
Imran Hegarty <imran.hegarty@telegraph.co.uk>
2016-05-31 04:25:59 +0100

    add-osgi-component-listchildren - fixing js issues and styling - more styling required

---
+++ bundle/src/main/java/com/whitbread/premierinn/components/listchildren/ListChildren.java
@@ -0,0 +1,41 @@
+package com.whitbread.premierinn.components.listchildren;
+
+import java.util.Iterator;
+
+import com.day.cq.wcm.api.Page;
+import org.apache.commons.lang3.StringUtils;
+
+import com.adobe.cq.sightly.WCMUse;
+
+/**
+ * Created by hegartyi on 29/05/2016.
+ */
+
+public class ListChildren extends WCMUse {
+
+	protected Page parentPage;
+	private Iterator<Page> pages;
+	private String pathfield;
+	private String value;
+
+	@Override
+	public void activate() {
+
+		pathfield = StringUtils.EMPTY;
+
+		pathfield = getProperties().get("path", "");
+
+		if (pathfield == null || pathfield.isEmpty()) {
+			this.parentPage = getCurrentPage();
+		} else {
+			this.parentPage = getPageManager().getPage(pathfield);
+		}
+
+		this.pages = this.parentPage.listChildren();
+
+	}
+
+	public Iterator<Page> getChildPages() {
+		return this.pages;
+	}
+}
